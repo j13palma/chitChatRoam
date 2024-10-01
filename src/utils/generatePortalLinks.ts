852367d9b7e209ca36bb68c123c5248063f1b1fc
julio palma <palma@onroot.io>
2024-10-01 07:04:06 -0400

    "update env var"

--- src/utils/generatePortalLinks.ts
+++ src/utils/generatePortalLinks.ts
@@ -6,7 +6,7 @@ import { headers } from 'next/headers';
 import { redirect } from 'next/navigation';
 import Stripe from 'stripe';
 
-const stripe = new Stripe(process.env.NEXT_PUBLIC_STRIPE_SECRET_KEY!, {
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
   apiVersion: '2024-06-20',
 });
 
